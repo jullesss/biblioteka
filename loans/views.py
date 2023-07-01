35e1315051cec422e922bfff18a9da6b326617bd
Leonardo Santos <kuhnnhuk@outlook.com>
2023-07-01 11:30:31 -0300

    feat: add views

--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import pagination
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import LoanSerializer
+from permissions.permissions import IsAdminOrOwner
+from .models import Loan
 
-# Create your views here.
+
+class LoanView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrOwner]
+    serializer_class = LoanSerializer
+    queryset = Loan.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
