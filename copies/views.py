6cb6c65cb7886799ef0581552ef6560e82f738cd
Claudinier Neto <devneto576@gmail.com>
2023-07-03 09:49:06 -0300

    fix: import in Copy views

--- copies/views.py
+++ copies/views.py
@@ -3,7 +3,7 @@ from .serializers import CopySerializer
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Copy
-from permissions.permissions import IsAdminOrReadOnly
+from permissions.permissions import IsAdminOrReadOnly, IsAdminOrOwner
 
 
 class CopyView(generics.ListCreateAPIView):
@@ -14,3 +14,10 @@ class CopyView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(book_id=self.kwargs.get("pk"))
+
+
+class CopyDetails(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrOwner]
+    queryset = Copy.objects.all()
+    lookup_url_kwarg = "book_id"
