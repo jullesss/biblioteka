0d3de419f504179772204bbdfd485691f64412ec
Leonardo Santos <kuhnnhuk@outlook.com>
2023-07-04 21:15:49 -0300

    fix: remake users migrations

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from copies import views as copy_views
 
 urlpatterns = [
     path("books/", views.BookView.as_view()),
-    path("books/<int:book_id>/", views.BookDetailView.as_view()),
+  #  path("books/<int:book_id>/", views.BookDetailView.as_view()),
     path("books/<int:book_id>/copies/", copy_views.CopyView.as_view()),
     path("books/<int:book_id>/copies/<int:copy_id>/", copy_views.CopyDetailsView.as_view()),
 ]
