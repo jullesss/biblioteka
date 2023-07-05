095ee102fa01a42e5e47a2f2a352c1c0df4699b3
suenefuzzo <suenequeiroz@hotmail.com>
2023-07-05 17:07:52 -0300

    feat: create copies through book post

--- books/models.py
+++ books/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=70)
+    title = models.CharField(max_length=150)
     author = models.CharField(max_length=70)
     pages_number = models.IntegerField()
-    pub_year = models.IntegerField()
-    pub_by = models.CharField(max_length=200)
+    publication_year = models.SmallIntegerField()
+    published_by = models.CharField(max_length=200)
     img_url = models.URLField(max_length=250)
     description = models.TextField(null=True)
 
