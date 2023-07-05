5095cc75335119016af45faf9db1e0008c20f586
suenefuzzo <suenequeiroz@hotmail.com>
2023-07-05 13:29:26 -0300

    fix: rewriting deleted BookDetailView

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from permissions.permissions import IsAdminOrReadOnly
+from permissions.permissions import IsAdminOrReadOnly, IsAdminTest
 
 
 class BookView(generics.ListCreateAPIView):
@@ -13,6 +13,21 @@ class BookView(generics.ListCreateAPIView):
     serializer_class = BookSerializer
 
     def perform_create(self, serializer):
-        serializer.save(user= self.request.user)
+        serializer.save(user=self.request.user)
 
 
+class BookDetailView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_url_kwarg = "book_id"
+
+
+class BookDetailView(generics.DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminTest]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_url_kwarg = "book_id"
