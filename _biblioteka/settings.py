cd6a601173eabc2a604247f848e1dc2da35ce9f3
Jorge Sousa <abel.jorge3@outlook.com>
2023-07-10 11:43:54 -0300

    feat: Insert settings.py mailing configuration

--- _biblioteka/settings.py
+++ _biblioteka/settings.py
@@ -155,3 +155,8 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
